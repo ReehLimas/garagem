96ac5bca666dfe12cd16abea7efdf4cbf3c7c59b
Reehlimas <reehlimasil08@gmail.com>
2025-05-05 16:54:34 -0300

    feat: Criação de APis

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,1 +1,5 @@
 from .user import UserSerializer
+from .acessorio import AcessorioSerializer
+from .cor import CorSerializers
+from .modelo import ModeloSerializers
+from .veiculo import VeiculoSerializers
